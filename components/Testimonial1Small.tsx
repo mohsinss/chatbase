adfc4e9010c280e16b35504538ac67d27cee65e9
Mohsin <92655551+mohsinss@users.noreply.github.com>
2024-12-30 15:40:01 +0300

    build fixing

--- components/Testimonial1Small.tsx
+++ components/Testimonial1Small.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-// A one or two sentences testimonial from a customer..
+// A one or two sentences testimonial from a customer.
 // Highlight the outcome for your customer (how did your product changed her/his life?) or the pain it's removing — Use <span className="bg-warning/25 px-1.5"> to highlight a part of the sentence
 const Testimonial1Small = () => {
   return (
