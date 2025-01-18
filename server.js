5771fa4e8c97dcddbf63d03478922e431a7c8797
codelover1226 <160652025+codelover1226@users.noreply.github.com>
2025-01-18 14:53:16 +0900

    Use express server for static files

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 import { createServer } from 'http';
 import next from 'next';
 import { Server } from 'socket.io';
+import express from 'express';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(async () => {
-    const server = createServer(handle);
+    const server = express();
     const io = new Server(server);
+
+    server.use(express.static('public'));
 
     io.on('connection', (socket) => {
         console.log("A user connected");
@@ -21,6 +24,10 @@ app.prepare().then(async () => {
         socket.on("disconnect", () => {
           console.log("A user disconnected");
         });
+    });
+
+    server.all('*', (req, res) => {
+        return handle(req, res);
     });
 
     server.listen(3000, (err) => {
