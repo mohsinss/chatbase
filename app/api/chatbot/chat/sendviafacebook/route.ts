0e3d22f5c7d8347f694e10d913877ae5564ab98c
codelover1226 <160652025+codelover1226@users.noreply.github.com>
2025-03-03 14:28:32 +0900

    Update sendviafacebook

--- app/api/chatbot/chat/sendviafacebook/route.ts
+++ app/api/chatbot/chat/sendviafacebook/route.ts
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/libs/next-auth";
 import ChatbotConversation from '@/models/ChatbotConversation';
 import { NextRequest, NextResponse } from 'next/server';
-import WhatsAppNumber from "@/models/WhatsAppNumber";
+import FacebookPage from "@/models/FacebookPage";
 import axios from "axios";
 
 export async function POST(req: Request) {
@@ -17,27 +17,29 @@ export async function POST(req: Request) {
     await connectMongo();
 
     // Fetch the existing WhatsAppNumber model
-    const whatsappNumber = await WhatsAppNumber.findOne({ display_phone_number: to });
-    if (!whatsappNumber) {
+    const facebookPage = await FacebookPage.findOne({ name: to });
+    if (!facebookPage) {
         // Respond with a 200 OK status
-        return NextResponse.json({ status: "Whatsapp Number doesn't registered to the site." }, { status: 200 });
+        return NextResponse.json({ status: "facebookPage doesn't registered to the site." }, { status: 200 });
     }
-    const chatbotId = whatsappNumber.chatbotId;
-    const phone_number_id = whatsappNumber.phoneNumberId;
+    const chatbotId = facebookPage.chatbotId;
+    const pageId = facebookPage.pageId;
 
     // send text msg to from number
-    const response2 = await axios.post(`https://graph.facebook.com/v22.0/${phone_number_id}/messages`, {
-        messaging_product: "whatsapp",
-        to: from,
-        text: {
-            body: text
-        }
+    const response2 = await axios.post(`https://graph.facebook.com/v22.0/${pageId}/messages?access_token=${facebookPage.access_token}`, {
+        messaging_type: "RESPONSE",
+        message: {
+            text: text
+        },
+        recipient: {
+            id: from
+        },
     }, {
         headers: { Authorization: `Bearer ${process.env.FACEBOOK_USER_ACCESS_TOKEN}` }
     });
 
     // Find existing conversation or create a new one
-    let conversation = await ChatbotConversation.findOne({ chatbotId, platform: "whatsapp", "metadata.from": from, "metadata.to": whatsappNumber.display_phone_number });
+    let conversation = await ChatbotConversation.findOne({ chatbotId, platform: "facebook", "metadata.from": from, "metadata.to": to });
     if (conversation) {
         // Update existing conversation
         conversation.messages.push({ role: "assistant", content: text, from: session.user.name });
