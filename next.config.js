49728534d004bbc410aa05667d6f9d29f35744d1
naturesky <naturesky0411@gmail.com>
2025-03-24 12:22:42 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -28,7 +28,41 @@ const nextConfig = {
     outputFileTracingIncludes: {
       '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto']
     }
-  }
+  },
+  swcMinify: true,
+  experimental: {
+    turbo: true, // Optional: faster builds
+  },
+  experimental: {
+    turbo: true, // Optional: faster builds
+    webpackMemoryOptimizations: true,
+    // webpackBuildWorker: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // if (config.cache && !dev) {
+    //   config.cache = Object.freeze({
+    //     type: 'memory',
+    //   })
+    // }
+    config.cache = false;
+    // Important: return the modified config
+    return config
+  },
 };
 
 export default nextConfig;
