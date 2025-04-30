37df01e3c7ed6e972695f3d17463a394c44812b4
Mohsin <92655551+mohsinss@users.noreply.github.com>
2025-04-30 20:19:49 +0700

    new structure

---
+++ app/data/blog/types.ts
@@ -0,0 +1,20 @@
+export interface BlogPost {
+  title: string;
+  excerpt: string;
+  content: string;
+  image: string;
+  author: string;
+  date: string;
+  readTime: string;
+  category: string;
+  tags: string[];
+  relatedPosts: {
+    id: string;
+    title: string;
+    image: string;
+  }[];
+}
+
+export interface BlogPosts {
+  [key: string]: BlogPost;
+} 
